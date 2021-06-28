81c7b5b7949bec9dfc6f2bbc2f1f01dd2ce9f888
Eylulyildiz <73103000+Eylulyildiz@users.noreply.github.com>
2021-06-29 02:22:57 +0300

    Add files via upload

---
+++ Tablo.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+</head>
+<body>
+
+<div class="container">
+
+<table class="table" border="0" cellpadding="5" cellspacing="10" align="center">
+	<thead class="thead-dark">
+	<tr>
+		<th>Ad</th>
+		<th>Soyad</th>
+		<th>Yas</th>
+		<th>Tel</th>
+		<th>e-mail</th>
+	</tr>
+</thead>
+
+	<tr>
+		<td align="center" colspan="2">tarcin</td>
+		
+		<td align="center">1</td>
+		<td align="center">00000</td>
+		<td align="center">tarconelli</td>
+	</tr>
+	<tr>
+		<td align="center" colspan="2"> envnoejvjrf </td>
+		
+		<td align="center"> lknvnejnvvnnve </td>
+		<td align="center"> lknvnejnvvnnve </td>
+		<td align="center"> lknvnejnvvnnve </td>
+	</tr>
+
+
+
+
+
+</table>
+
+</div>
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
